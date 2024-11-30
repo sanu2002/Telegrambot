f6608656f7a9417a3a24ca77ef242bd03b3b4a65
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2024-12-01 00:30:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import requests
 MESSAGE = 'Hello from GitHub Actions!'
+# Get the Telegram bot token and chat ID from GitHub Secrets (stored as environment variables)
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')  # Access the secret from GitHub Secrets
+CHAT_ID = os.getenv('CHAT_ID')  # Access the secret from GitHub Secrets
+
 
 url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
 payload = {'chatid': CHAT_ID, 'text': MESSAGE}
