f9035d48ac4c760cbe52bef73cc62b79f7b64954
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2024-12-01 00:25:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import requests
-TOKEN = "6662820897:AAF_IaDV4a2-ZCGiCF4jrRTvTK4XbmgpzMc"
 CHATID = '6662820897'
 MESSAGE = 'Hello from GitHub Actions!'
 
-url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
 payload = {'chatid': CHATID, 'text': MESSAGE}
 
 response = requests.post(url, json=payload)
